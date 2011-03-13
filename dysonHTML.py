a4c4bf52f2cb9b40b6ce2b924e55e9b0cf09b352
tservi <jeantinguelyawais@gmail.com>
2011-03-13 16:29:47 +0000

    Step four : sending an email.

--- dysonHTML.py
+++ dysonHTML.py
@@ -152,6 +152,107 @@ for e in eventids:
     #print( content ) 
     myEvents.append( myE )
 
+# fourth step
+# sending the mails : one email for every events
+# install this program on windows http://www.softstack.com/freesmtp.html
+user             = ''
+password         = ''
+text             = ''
+text            += '<html>\n<head>\n<title>New event</title>\n</head>\n<body>\n'
+text            += 'Email de validation des events depuis myswitzerland.com<br/>\n'
+text            += "<form method='post' action='http://www.suisseevents.ch/inject.php'>\n"
+for num in myEvents:
+    event        = myE[ num ]
+    eid          = 'myswitzerlandcom'
+    text        += "<table border='1' style='width: 100%'>\n"
+    text        += "<tr>\n"
+    text        += "<td> "
+    if 'event_id' in event:
+        text    += event[ 'event_id' ]
+        eid     += event[ 'event_id' ]
+    text    += "<input type='hidden' name='event_id[]' value='" + eid + "' />"
+    text        += "</td>"
+    text        += "<td> "
+    if 'title_fr' in event and len ( event[ 'title_fr' ] ) > 1 :
+        text    += event[ 'title_fr' ] 
+    text        += "</td>"
+    text        += "<td> "
+    if 'title_de' in event and len ( event[ 'title_de' ] ) > 1 :
+        text    += event[ 'title_de' ]
+    text        += "</td>"
+    text        += "<td> "
+    if 'title_it' in event and len ( event[ 'title_it' ] ) > 1 :
+        text    += event[ 'title_it' ]
+    text        += "</td>"
+    text        += "<td> "
+    if 'title_en' in event and len ( event[ 'title_en' ] ) > 1 :
+        text    += event[ 'title_en' ]
+    text        += "</td>"
+    text        += "</tr>\n"
+
+    for key in event:
+        if key != 'event_id' and key != 'fk_category' :
+            text += "<tr>\n"
+            text += "<td>\n"
+            text += str( key ) + ' : '
+            text += "</td>\n"
+            text += "<td colspan='4'>\n"
+            text += "<textarea name='" + eid + '-' + key + "' style='width: 100%' >"
+            text += event[ key ]
+            text += "</textarea>"
+            text += "</td>\n"
+            text += "</tr>\n"
+
+    if 'fk_category' in event :
+            text += "<tr>\n"
+            text += "<td>\n"
+            text += "Categories  : "
+            text += "</td>\n"
+            text += "<td colspan='4'>\n"
+            text += "<select name='" + eid + "-fk_category' style='width: 100%' >"
+            cats  = { 'A' : 'Concert - Discotheque - Festival - Jazz - World Music' ,
+                     'B' : 'Spectacles - Theatres - Opera - Comedies ' ,
+                     'C' : 'Divers' ,
+                     'D' : 'Exposition - Foires - Conferences - Congres - Seminaires ',
+                     'E' : 'Visites commentes - Manifestations ',
+                     'F' : 'Festival' ,
+                     'G' : 'Expositon - Foires' ,
+                     'H' : 'Sport' ,
+                     'I' : 'Musee' ,
+                     'J' : 'Manifestation' ,
+                     'K' : 'Gastronomie' ,
+                     'O' : 'Divers' , 
+                     }  
+            for option in cats :
+                text += "<option value='" + option
+                text += "'"
+                if event[ 'fk_category' ] == option :
+                    text += " selected "
+                text += ">" + cats[ option ] + "</option>"
+            text += "</select>"
+            text += "</td>\n"
+            text += "</tr>\n"
+    
+    text        += "<tr><td colspan='5'><input type='submit' value='injecter dans suisseevents.ch'></td></tr>\n"
+    text        += "</table>\n"
+    text        += "<br/><br/>\n"
+text        += "</form>\n"
+text            += '</body>\n'
+msg              = MIMEText( text, 'html', 'UTF-8')
+now              = datetime.datetime.today()
+msg['Subject']   = 'Aspiration HTML de myswitzerland.com ' + str( now )
+msg['From']      = "info@t-servi.com"
+msg['Reply-to']  = "info@t-servi.com"
+msg['To']        = "aeschlimann.charles@gmail.com"
+#print( msg )
+s                = smtplib.SMTP( "ca-dev.com" )
+#s.set_debuglevel( 1 )
+s.ehlo()
+s.login( user, password )
+s.sendmail( "<info@t-servi.com>" , "<aeschlimann.charles@gmail.com>" ,msg.as_string() )
+s.close()
+print ( "Vous devez avoir recu un email dans votre boite au lettre!  ")
+
 #print( myEvents )
 dyson = """
 **********************************************************************
